cd27cbde603c6c6360ef13a315a283345e32e6b1
lkssn <>
2023-12-27 01:59:39 +0100

    day17: basic dijkstra

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,31 +1,19 @@
-use std::{vec, collections::BinaryHeap, cmp::Ordering};
+use std::fmt;
+
+
 
 fn main() {
     let input = std::fs::read_to_string("day17/data/example.txt").expect("failed to read file");
-    let map = Map::parse(&input);
+    let mut map = Map::parse(&input);
+    let s = Point::new(0, 0);
+    let t = Point::new(map.w-1, map.h-1);
+    let distance = map.find_optimal_heat_loss(s, t);
+
+    println!("distance: {}", distance);
     map.print();
-
-    let path = Path {
-        points: vec![
-            Point { x: 0, y: 0 },
-            Point { x: 1, y: 0 },
-            Point { x: 1, y: 1 },
-        ]
-    };
-    let path_heat_loss = map.path_heat_loss(&path);
-    println!("path_heat_loss: {}", path_heat_loss);
-
-    // TODO: Think of the Ansatz:
-    // 1. If the path restriction would not be there, this would just be Djikstra
-    // 2. Can path restriction also be incorporated into Djikstra?
-    //    I am not 100% sure but the following might work (just test it!):
-    //    While doing normal Djikstra keep track of the amount of times that we move in a straight line consecutively. (left/right: set 0, straight: increment)
-    //    If counter >= 3: do not consider the straight edge.
-
 }
 
 struct Tile {
-    position: Point,
     heat_loss: i64,
     marked: bool,
     distance: i64,
@@ -33,39 +21,46 @@ struct Tile {
 }
 
 impl Tile {
-    fn new(x: i64, y: i64, heat_loss: i64) -> Tile {
-        Tile { position: Point { x, y }, heat_loss, marked: false, distance: i64::MAX, parent: None }
+    fn new(heat_loss: i64) -> Tile {
+        Tile { heat_loss, marked: false, distance: i64::MAX, parent: None }
     }
 }
 
-impl Ord for Tile {
-    fn cmp(&self, other: &Self) -> Ordering {
-        // this generates the reverse ordering, so we can use the simple MaxHeap as a MinHeap!
-        self.heat_loss.cmp(&other.heat_loss).reverse()
-    }
-}
-
-impl PartialOrd for Tile {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for Tile {
-    fn eq(&self, other: &Self) -> bool {
-        (self.position.x, self.position.y) == (other.position.x, other.position.y)
-    }
-}
-
-impl Eq for Tile {}
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Point {
     x: i64,
-    y: i64
+    y: i64,
+}
+
+#[derive(Debug)]
+struct Path {
+    points: Vec<Point>
+}
+
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Direction {
+    fn print(&self) {
+        let c = match self {
+            Self::North => '^',
+            Self::East =>  '>',
+            Self::South => 'v',
+            Self::West =>  '<',
+        };
+        print!("{}", c);
+    }
 }
 
 impl Point {
+    fn new(x: i64, y: i64) -> Point {
+        Point { x, y }
+    }
+
     fn neighbourhood(&self) -> Vec<Point> {
         vec![
             Point { x: self.x-1, y: self.y   },
@@ -74,10 +69,27 @@ impl Point {
             Point { x: self.x,   y: self.y+1 },
         ]
     }
-}
 
-struct Path {
-    points: Vec<Point>
+    fn get_direction(&self, next: &Self) -> Direction {
+        let d = self.x.abs_diff(next.x) + self.y.abs_diff(next.y);
+        if d != 1 {
+            panic!("get_direction: not neighbours {:?}, {:?}", self, next);
+        }
+
+        if self.y > next.y {
+            return Direction::North;
+        }
+
+        if self.y < next.y {
+            return Direction::South;
+        }
+
+        if self.x < next.x {
+            return Direction::East;
+        }
+
+        return Direction::West;
+    }
 }
 
 struct Map {
@@ -90,10 +102,10 @@ impl Map {
     fn parse(s: &str) -> Map {
         let mut map = Map { w: 0, h: 0, tiles: vec![] };
 
-        for (y, line) in s.lines().enumerate() {
-            for (x, c) in line.chars().enumerate() {
+        for line in s.lines() {
+            for c in line.chars() {
                 let heat_loss = c as i64 - '0' as i64;
-                let tile = Tile::new(x as i64, y as i64, heat_loss);
+                let tile = Tile::new(heat_loss);
                 map.tiles.push(tile);
             }
             map.h += 1;
@@ -106,88 +118,126 @@ impl Map {
     fn print(&self) {
         for y in 0..self.h {
             for x in 0..self.w {
-                print!("{}", self.get(x, y).heat_loss);
+                let p = Point::new(x, y);
+                let tile = self.get(p);
+
+                if let Some(parent) = tile.parent {
+                    let direction = parent.get_direction(&p);
+                    direction.print();
+                } else {
+                    print!("{}", tile.heat_loss);
+                }
             }
             println!();
         }
     }
 
-    fn get(&self, x: i64, y: i64) -> &Tile {
-        let index = y * self.w + x;
+    fn print_path(&self, path: &Path) {
+        for y in 0..self.h {
+            for x in 0..self.w {
+                let p = Point::new(x, y);
+                let tile = self.get(p);
+
+                if path.points.contains(&p) {
+                    if let Some(parent) = tile.parent {
+                        let direction = parent.get_direction(&p);
+                        direction.print();
+                    } else {
+                        println!("p has no parent: {:?}", p);
+                    }
+                } else {
+                    print!("{}", tile.heat_loss);
+                }
+            }
+            println!();
+        }
+    }
+
+    fn get(&self, p: Point) -> &Tile {
+        let index = p.y * self.w + p.x;
         &self.tiles[index as usize]
     }
 
-    fn get_mut(&mut self, x: i64, y: i64) -> &mut Tile {
-        let index = y * self.w + x;
+    fn get_mut(&mut self, p: Point) -> &mut Tile {
+        let index = p.y * self.w + p.x;
         &mut self.tiles[index as usize]
     }
 
-    fn path_heat_loss(&self, path: &Path) -> i64 {
-        let mut sum = 0;
-
-        for point in path.points.iter().skip(1) {
-            let tile = self.get(point.x, point.y);
-            sum += tile.heat_loss;
+    fn remove_minimal_point(&self, v: &mut Vec<Point>) -> Point {
+        if v.is_empty() {
+            panic!("find_minimal_distance: empty");
         }
 
-        sum
+        let mut min_index = 0;
+
+        for (i, p) in v.iter().enumerate() {
+            let d = self.get(*p).distance;
+            let min_d = self.get(v[min_index]).distance;
+
+            if d < min_d {
+                min_index = i;
+            }
+        }
+
+        v.remove(min_index)
     }
 
-    fn find_optimal_path(&mut self, s: Point, t: Point) -> Path {
-        let mut path = Path { points: vec![] };
-        let mut boundary = BinaryHeap::new();
-        self.get_mut(0, 0).marked = true;
-        self.get_mut(1, 0).parent = Some(Point { x: 0, y: 0 });
-        self.get_mut(0, 1).parent = Some(Point { x: 0, y: 0 });
-        boundary.push(self.get(1, 0));
-        boundary.push(self.get(0, 1));
-        boundary.peek_mut()
+    fn find_optimal_heat_loss(&mut self, s: Point, t: Point) -> i64 {
+        let mut boundary = vec![];
+        self.get_mut(s).distance = 0;
+        self.get_mut(s).marked = true;
+        boundary.push(s);
 
         // Do a 3-Partition of the set of nodes:
-        // 1. Known: We know the shortest path to these nodes.
-        // 2. Boundary: Candidate nodes for further exploration of the unknown.
-        // 3. Unknown: We have not seen these nodes yet.
-        let mut consecutive_counter = 0;
+        // 1. Known: We know the shortest path to these nodes. -> Known = {}
+        // 2. Boundary: Candidate nodes for further exploration of the unknown. -> Boundary = {s}
+        // 3. Unknown: We have not seen these nodes yet. -> Unknown = V
+
+        // TODO: implement consecutive counter
         loop {
+            println!("{:?}", boundary);
             // extend shortest path tree with one new node
-            let next = boundary.pop().unwrap();
-            let p = next.position;
-            self.get_mut(p.x, p.y).marked = true;
-            
-            let neighbourhood = p.neighbourhood();
+            let next = self.remove_minimal_point(&mut boundary);
+            if next == t {
+                break;
+            }
+
+            // update the boundary
+            let neighbourhood = next.neighbourhood();
+            let next_d = self.get(next).distance;
             for neighbour in neighbourhood {
                 if !(0..self.w).contains(&neighbour.x) || !(0..self.h).contains(&neighbour.y) {
                     continue;
                 }
 
-                let tile = self.get(neighbour.x, neighbour.y);
-                let distance = next.distance + tile.heat_loss;
-                tile.distance = tile.distance.min(next.distance + tile.heat_loss);
-                
+                let tile = self.get_mut(neighbour);
+
+                if next_d + tile.heat_loss < tile.distance {
+                    tile.parent = Some(next);
+                    tile.distance = next_d + tile.heat_loss;
+                }
+
                 if !tile.marked {
                     tile.marked = true;
-                    boundary.push(tile);
+                    boundary.push(neighbour);
                 }
             }
-
-            // TODO: later also consider the consecutive counter. Right now ignore it.
-
-
-
-            // TODO: search until t is found
-            // TODO: impl modified dijkstra
-            panic!("dijkstra not implemented");
         }
 
-        path
-    }
+        let mut path = Path { points: vec![] };
+        let mut iter = t;
+        loop {
+            path.points.push(iter);
+            if let Some(parent) = self.get(iter).parent {
+                iter = parent;
+            } else {
+                break;
+            }
+        }
+        path.points.reverse();
+        println!("path: {:?}", path);
+        self.print_path(&path);
 
-    fn find_optimal_heat_loss(&mut self) -> i64 {
-        let w  = self.w;
-        let h = self.h;
-        let s = Point { x: 0, y: 0 };
-        let t = Point { x: w-1, y: h-1 };
-        let path = self.find_optimal_path(s, t);
-        self.path_heat_loss(&path)
+        self.get(t).distance
     }
 }
